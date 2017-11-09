f042b5c4add072d011a2cbfe94a2bc1a033b4530
Scotow <contact@scotow.com>
2017-11-09 22:51:23 +0100

    Minify front-end js.

--- public/js/upload/app.js
+++ public/js/upload/app.js
@@ -12,7 +12,6 @@ $(function() {
     // Cookie.
     var prettyCookie = Cookies.get('pretty');
     var pretty = prettyCookie === undefined ? 1 : parseInt(prettyCookie);
-    console.log(pretty);
     $pretty.prop('checked', pretty).change(function() {
         pretty = this.checked ? 1 : 0;
         Cookies.set('pretty', pretty, {expires: 365});
