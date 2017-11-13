f6d59d55a5f22aea10064c4a30f6c3869555b4a0
Lopez Benjamin <benjamin-lopez@hotmail.fr>
2017-11-13 02:12:25 +0100

    Revert "Revert "Set NodeJS branch as master branch""

---
+++ app.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+// Setup.
+const PORT = process.env.PORT || (process.env.DEV ? 5003 : 4003);
+
+// Imports.
+// Basic modules.
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+// Database.
+const database = require('./lib/database.js');
+
+// Web server.
+const express = require('express');
+const download = require('./routes/download.js');
+const upload = require('./routes/upload.js');
+const revoke = require('./routes/revoke.js');
+const info = require('./routes/info.js');
+const utils = require('./lib/utils.js');
+const error = require('./lib/error.js');
+
+const app = express();
+app.set('trust proxy', true);
+app.set('x-powered-by', false);
+app.set('json spaces', '\t');
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', download);
+app.use('/', upload);
+app.use('/', revoke);
+app.use('/', info);
+
+app.all('*', (req, res) => {
+    utils.displayError(req, res, error.fromCode(404));
+});
+
+start();
+
+async function start() {
+    try {
+        const connection = await database.connect();
+        console.log(`Connected to mySQL server (${connection.threadId}).`);
+        await initUploadsFolder();
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}.`);
+        });
+    } catch(error) {
+        console.error(`Error starting sandrop: ${error.stack}`);
+    }
+}
+
+async function initUploadsFolder() {
+    const UPLOAD_PATH = path.join(os.tmpdir(), 'uploads');
+    if(fs.existsSync(UPLOAD_PATH)) {
+        const fileRegex = /^\d+$/;
+        const files = fs.readdirSync(UPLOAD_PATH).filter(file => fileRegex.test(file));
+        if(files.length) {
+            const filesToDelete = (await database.shouldBeDelete(files));
+            filesToDelete.forEach(file => fs.unlinkSync(path.join(UPLOAD_PATH, String(file.id))));
+        }
+    } else {
+        fs.mkdirSync(UPLOAD_PATH);
+    }
+}
---
+++ routes/download.js
@@ -0,0 +1,110 @@
+// Imports.
+// Basic modules.
+const path = require('path');
+const fs = require('fs');
+
+// Utils.
+const utils = require('../lib/utils.js');
+const archive = require('../lib/archive.js');
+
+// Database.
+const database = require('../lib/database.js');
+
+// Express.
+const router = require('express').Router();
+
+router.get(/^\/(?:(a|archive|f|g|i|t|v|z)\/)?([a-zA-Z0-9]{6}|[a-z]+(?:-[a-z]+){2})$/, (req, res) => {
+    const shortcut = req.params[0];
+    const alias = req.params[1];
+    if(shortcut === 'a' || shortcut === 'archive') {
+        prepareArchive(res, alias);
+    } else {
+        database.getFile(alias)
+        .then(utils.checkFileExists)
+        .then(file => {
+            let type;
+            switch(shortcut) {
+                case 'f':
+                    type = file.type;
+                    break;
+                case 't':
+                    type = 'text/plain';
+                    break;
+                case 'i':
+                    type = 'image';
+                    break;
+                case 'g':
+                    type = 'image/gif';
+                    break;
+                case 'v':
+                    type = 'video';
+                    break;
+                case 'z':
+                    sendArchive(res, file);
+                    return;
+                default:
+                    res.download(file.path, file.name, error => filesDownloaded(error, file));
+                    return;
+            }
+            res.sendFile(file.path, {
+                headers: {
+                    'Content-Type': type,
+                    'Content-Disposition': `inline; filename="${file.name}"`
+                }
+            }, error => filesDownloaded(error, file));
+        })
+        .catch(error => utils.displayError(req, res, utils.buildError(error.message), 404));
+    }
+});
+
+router.get(/^\/((?:[a-zA-Z0-9]{6}|[a-z]+(?:-[a-z]+){2})(?:\+(?:[a-zA-Z0-9]{6}|[a-z]+(?:-[a-z]+){2}))+)$/, (req, res) => {
+    Promise.all(req.params[0].split('+').map(alias => database.getFile(alias).then(utils.checkFileExists)))
+    .then(files => {
+        if(files.length) {
+            sendArchive(res, files);
+        } else {
+            utils.displayError(req, res, utils.buildError('An error has occured while fetching your file(s).'), 404);
+        }
+    })
+    .catch(error => utils.displayError(req, res, utils.buildError(error.message), 404));
+});
+
+function prepareArchive(res, alias) {
+    database.getArchiveFiles(alias)
+    .then(files => Promise.all(files.map(file => utils.checkFileExists(file).catch(() => null))))
+    .then(files => {
+        const existingFiles = files.filter(Boolean);
+        if(existingFiles.length) {
+            sendArchive(res, existingFiles);
+        } else {
+            utils.displayError(req, res, utils.buildError('All files of this archive have expired.'), 404);
+        }
+    })
+    .catch(error => utils.displayError(req, res, utils.buildError(error.message), 404));
+}
+
+function sendArchive(res, files) {
+    archive.createZip(files)
+    .then(archivePath => {
+        res.download(archivePath, 'files.zip', error => {
+            filesDownloaded(error, files);
+            fs.unlink(archivePath, error => error && console.error('Error while unlinking archive file.', error, archivePath));
+        });
+    })
+    .catch(error => {
+        utils.displayError(req, res, utils.buildError(error.message), 404);
+    });
+}
+
+function filesDownloaded(error, files) {
+    if(error) {
+        console.error('Error while uploading archive.', error, archivePath);
+        return;
+    }
+
+    if(!Array.isArray(files)) files = [files];
+    database.filesDownloaded(files.map(file => file.id))
+    .catch(error => console.error('Error while updating files download count.', error));
+}
+
+module.exports = router;
