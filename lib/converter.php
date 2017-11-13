00540c0f2c322a266dfc6a9b74289004f5f73c6a
Scotow <contact@scotow.com>
2017-11-13 02:19:26 +0100

    Remove php code.

--- lib/converter.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function human_filesize($bytes, $decimals = 2) {
-    $sz = 'BKMGTP';
-    $factor = intval(floor((strlen($bytes) - 1) / 3));
-    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . $sz[$factor];
-}
